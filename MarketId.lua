24edf43eab21b1c979d1ef16f74ce4e89bdc557d
Brandon Burrus <brandon@brandonburrus.com>
2015-08-31 00:58:23 -0700

    game.Workspace.MarketId
    Just a Module that contains asset ID's for market handling.

---
+++ MarketId.lua
@@ -0,0 +1,19 @@
+return {
+	FiftyCredits = 25081645,
+	OneHundredCredits = 25081785,
+	FiveHundredCredits = 25081835,
+	OneThousandCredits = 25081845,
+	FiveThousandCredits = 25081866,
+	TenThousandCredits = 25081872,
+	FiftyThousandCredits = 25081901,
+	
+	BlankStart = 272927570,
+	ColorSpectrum = 272945612,
+	Donator = 272946622,
+	DrinksAround = 272944582,
+	Experienced = 272943296,
+	IncreasedOdds = 272933195,
+	RollingWealth = 272940261,
+	
+	ResetStats = 25081932
+}
